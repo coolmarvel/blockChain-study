449decc0d8a959a32c56f3bdde450306daf77261
coolmarvel <marvel97@naver.com>
2022-01-05 15:55:53 +0900

    block

--- src/block.js
+++ src/block.js
@@ -40,15 +40,15 @@ function getLastBlock() {
 function createGenesisBlock() {
   const version = "1.0.0"//
   const index = 0
-  const time = 1630907567
+  const time = 1641351600
   const previousHash = '0'.repeat(64)
-  const body = ['hello block']
+  const body = ['The Time has come']
 
   const tree = merkle('sha256').sync(body)
   const root = tree.root() || '0'.repeat(64)
 
   //09.08추가
-  const difficulty = 0
+  const difficulty = 1
   const nonce = 0
 
   const header = new BlockHeader(version, index, previousHash, time, root, difficulty, nonce)
@@ -117,7 +117,7 @@ function hashMatchDifficulty(hash, difficulty) {
   const prefix = "0".repeat(difficulty);
 
   //높으면 높을수록 조건을 맞추기가 까다로워짐(nonce값과 time값이 바뀌면서 암호화값이 달라진다.)
-  return hashBinary.startsWith(prefix)
+  return hash.startsWith(prefix)
 
 }
 
